e5f86cf5a459ded8d5bfa9202605b33235c80f35
dmdv <dimos-d@yandex.ru>
2017-06-27 02:44:37 +0300

    Added server

--- WebApp/Example/main.go
+++ WebApp/Example/main.go
@@ -1,9 +1,19 @@
 package main
 
+import (
+	"net/http"
+	"fmt"
+)
+
 type defaultHandler struct {
 	greeting string
 }
 
-func main() {
+func (handler defaultHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte (fmt.Sprintf("%v, world!", handler.greeting)))
+}
 
+func main() {
+	http.Handle("/", &defaultHandler{greeting:"Hello"})
+	http.ListenAndServe(":8001", nil)
 }
