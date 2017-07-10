f77d0aaf8beecfe567ed3a8b3df45a6fd1eda7cd
dmdv <dimos-d@yandex.ru>
2017-07-10 08:58:25 +0300

    Minor

---
+++ WebApp/Templates/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"html/template"
+	"fmt"
+	"os"
+)
+
+func main(){
+	templateString := `Lemonade Stand Supply`
+	t, err := template.New("title").Parse(templateString)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = t.Execute(os.Stdout, nil)
+
+	if err != nil{
+		fmt.Println(err)
+	}
+}
